67872d29630276b47fff60b05c57f7d26b067b52
Sebastian Friston <sebastian.friston@gmail.com>
2019-02-13 22:51:08 +0000

    initial commit

---
+++ Editor/VertexToolsWindow.cs
@@ -0,0 +1,212 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.Runtime.InteropServices;
+
+public class VertexTool : EditorWindow
+{
+    // Add menu named "My Window" to the Window menu
+    [MenuItem("Tools/Vertex Colours")]
+    static void Init()
+    {
+        // Get existing open window or if none, make a new one:
+        VertexTool window = (VertexTool)EditorWindow.GetWindow(typeof(VertexTool));
+        window.Show();
+    }
+
+    private enum Flags : uint
+    {
+        Selected = 1,
+        Backfacing = 2
+    }
+
+    // Rendering resources
+    private Material pointMaterial;
+    private ComputeBuffer gpu_vertices;
+    private ComputeBuffer gpu_normals;
+    private ComputeBuffer gpu_colours;
+    private ComputeBuffer gpu_flags;
+    private uint[] cpu_flags;
+
+    private List<int> selected;
+
+    // Visualisation properties
+    private bool cullBackfacing = true;
+    private float size = 0.2f;
+
+    // Tool settings
+    private float brushSize = 10f;
+
+    // State properties
+    private bool painting = false;
+
+    void OnEnable()
+    {
+        SceneView.onSceneGUIDelegate += this.OnSceneGUI;
+
+        if (pointMaterial == null)
+        {
+            pointMaterial = new Material(Shader.Find("VertexTools/PointShader"));
+        }
+        if (gpu_vertices == null)
+        {
+            gpu_vertices = new ComputeBuffer((int)ushort.MaxValue, Marshal.SizeOf(typeof(Vector3)));
+        }
+        if (gpu_normals == null)
+        {
+            gpu_normals = new ComputeBuffer((int)ushort.MaxValue, Marshal.SizeOf(typeof(Vector3)));
+        }
+        if (gpu_colours == null)
+        {
+            gpu_colours = new ComputeBuffer((int)ushort.MaxValue, Marshal.SizeOf(typeof(Vector3)));
+        }
+        if (gpu_flags == null)
+        {
+            gpu_flags = new ComputeBuffer((int)ushort.MaxValue, Marshal.SizeOf(typeof(uint)));
+            cpu_flags = new uint[gpu_flags.count];
+        }
+
+        if(selected == null)
+        {
+            selected = new List<int>();
+        }
+    }
+
+    void OnDisable()
+    {
+        SceneView.onSceneGUIDelegate -= this.OnSceneGUI;
+
+        gpu_vertices.Dispose();
+        gpu_vertices = null;
+
+        gpu_normals.Dispose();
+        gpu_normals = null;
+
+        gpu_colours.Dispose();
+        gpu_colours = null;
+
+        gpu_flags.Dispose();
+        gpu_flags = null;
+
+        painting = false;
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Label("Settings", EditorStyles.boldLabel);
+        size = EditorGUILayout.Slider("Point Size", size, 0.01f, 10f);
+        cullBackfacing = EditorGUILayout.Toggle("Cull Backfacing", cullBackfacing);
+
+        brushSize = EditorGUILayout.Slider("Brush Size", brushSize, 0.1f, 100f);
+
+        EditorGUILayout.LabelField("Selected: " + selected.Count);
+        EditorGUILayout.LabelField("Painting: " + painting);
+    }
+
+    void OnSceneGUI(SceneView sceneView)
+    {
+        if(Selection.activeGameObject == null)
+        {
+            return;
+        }
+
+        var gameobject = Selection.activeGameObject;
+
+        Mesh mesh = null;
+
+        var renderer = gameobject.GetComponent<MeshFilter>();
+        if (renderer != null)
+        {
+            mesh = renderer.sharedMesh;
+        }
+
+        var skinnedrenderer = gameobject.GetComponent<SkinnedMeshRenderer>();
+        if( skinnedrenderer != null)
+        {
+            mesh = skinnedrenderer.sharedMesh;
+        }
+
+        if(mesh == null)
+        {
+            return;
+        }
+
+        var transform = gameobject.transform.localToWorldMatrix;
+
+        gpu_vertices.SetData(mesh.vertices);
+        gpu_normals.SetData(mesh.normals);
+        gpu_colours.SetData(mesh.colors);
+
+        pointMaterial.SetBuffer("points", gpu_vertices);
+        pointMaterial.SetBuffer("normals", gpu_normals);
+        pointMaterial.SetBuffer("colours", gpu_colours);
+        //pointMaterial.SetBuffer("flags", gpu_flags);
+        Graphics.SetRandomWriteTarget(1, gpu_flags, true);
+
+        pointMaterial.SetMatrix("ObjectToWorld", transform);
+
+        pointMaterial.SetFloat("Size", size);
+
+        var mousePosition = Event.current.mousePosition;
+        mousePosition.y = sceneView.camera.pixelHeight - mousePosition.y;
+        var tool = new Vector4(mousePosition.x, mousePosition.y, brushSize, painting ? 1 : -1);
+        pointMaterial.SetVector("Tool", tool);
+
+        pointMaterial.SetInt("CullBackfacing", cullBackfacing ? 1 : 0);
+
+        pointMaterial.SetPass(0);
+        Graphics.DrawProcedural(MeshTopology.Points, mesh.vertexCount);
+        
+        Handles.BeginGUI();
+
+        if (mouseOverWindow is SceneView)
+        {
+            if (Event.current.type == EventType.Repaint)
+            {
+                DrawCircleBrush(Color.white, brushSize);
+            }
+        }
+
+        Handles.EndGUI();
+
+        if(Event.current.isKey && Event.current.keyCode == KeyCode.LeftControl) // our left mouse button.
+        {
+            switch (Event.current.type) 
+            {
+                case EventType.KeyDown:
+                    painting = true;
+                    break;
+                case EventType.KeyUp:
+                    painting = false;
+                    break;
+            }
+        }
+
+        if (painting)   
+        {
+            gpu_flags.GetData(cpu_flags);
+
+            for (int i = 0; i < mesh.vertexCount; i++)
+            {
+                if (((Flags)cpu_flags[i] & Flags.Selected) > 0)
+                {
+                    selected.Add(i);
+                }
+            }
+        }
+
+        sceneView.Repaint();
+    }
+
+    //https://forum.unity.com/threads/how-can-i-draw-a-circle-around-the-mouse-pointer-on-scene-view.474273/
+    private static void DrawCircleBrush(Color color, float size)
+    {
+        Handles.color = color;
+        // Circle
+        Handles.CircleHandleCap(0, Event.current.mousePosition, Quaternion.identity, size, EventType.Repaint);
+        // Cross Center
+        Handles.DrawLine(Event.current.mousePosition + Vector2.left, Event.current.mousePosition + Vector2.right);
+        Handles.DrawLine(Event.current.mousePosition + Vector2.up, Event.current.mousePosition + Vector2.down);
+    }
+}
---
+++ PointShader.shader
@@ -0,0 +1,172 @@
+﻿Shader "VertexTools/PointShader"
+{
+	Properties
+	{
+	}
+	SubShader
+	{
+		Tags{ "RenderType" = "Opaque" }
+		LOD 100
+
+		ZTest Always
+		ZWrite Off
+		Blend SrcAlpha OneMinusSrcAlpha
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			#pragma geometry geom
+			#pragma target 5.0
+		
+			#include "UnityCG.cginc"
+
+			StructuredBuffer<float3> points;
+			StructuredBuffer<float3> normals;
+			StructuredBuffer<float4> colours;
+
+			RWStructuredBuffer<uint> flags : register(u1);
+
+			float4x4 ObjectToWorld;	
+
+			float Size;
+			float4 Tool;
+			int CullBackfacing;
+
+			struct v2g
+			{
+				float4 vertex : SV_POSITION;
+				float4 colour : COLOR;
+				int index : TEXCOORD1;
+				uint flags : TEXCOORD2;
+			};
+
+			struct g2f
+			{
+				float4 vertex : SV_POSITION;
+				float4 colour : COLOR;
+				float4 screenpos : TEXCOORD0;
+				int index : TEXCOORD1;
+				uint flags : TEXCOORD2;
+			};
+
+			// because unity_ObjectToWorld is not set properly when using DrawProcedural yet we have to do these explicitly
+
+			inline float4 ObjectToClipPos(in float3 pos)	
+			{
+				return mul(UNITY_MATRIX_VP, mul(ObjectToWorld, float4(pos, 1.0)));
+			}
+
+			inline float4 ObjectToWorldDir(in float3 dir)
+			{
+				return mul(ObjectToWorld, float4(dir, 0.0));
+			}
+
+			v2g vert(uint id : SV_VertexID)
+			{
+				v2g o;
+				o.vertex = ObjectToClipPos(points[id]);
+				o.colour = colours[id];
+				o.index = id;
+				o.flags = 0;
+
+				o.colour.a = 1;
+
+				// execute the selection/painting tool
+
+				// how to use computescreenpos properly: https://forum.unity.com/threads/what-does-the-function-computescreenpos-in-unitycg-cginc-do.294470/
+
+				float4 screenpos = ComputeScreenPos(o.vertex);
+				float2 screen = (screenpos.xy / screenpos.w) * _ScreenParams.xy;
+
+				if (length(screen - Tool.xy) < Tool.z)
+				{
+					if (Tool.w > 0) 
+					{
+						o.colour = float4(1, 0, 0, 1) - (o.colour * 0.9) + (o.colour * 0.9);
+					}
+					else
+					{
+						o.colour = float4(1, 1, 1, 1) - (o.colour * 0.9) + (o.colour * 0.9);
+					}
+				
+					o.flags = o.flags | 1;
+				}
+
+				// check for backfacing vertices
+
+				float3 worldNormal = ObjectToWorldDir(normals[id]);
+				float3 forward = mul((float3x3)unity_CameraToWorld, float3(0, 0, 1));	// https://answers.unity.com/questions/192553/camera-forward-vector-in-shader.html
+
+				if (dot(worldNormal, forward) > 0)
+				{
+					o.flags = o.flags | 2;
+				}
+
+				// modulate the colour based on the normal
+
+				o.colour = o.colour * max(-dot(worldNormal, forward), 0.3);
+				
+				return o;
+			}
+
+			[maxvertexcount(4)]
+			void geom(point v2g p[1], inout TriangleStream<g2f> triStream)
+			{
+				float3 right = float3(1, 0, 0);
+				float3 up = float3(0, 1.5, 0);
+				float halfS = 0.015f * Size;
+				g2f v[4];
+
+				// apply the perspective correction now so the box will be sized in clip space, and so consistent on screen no matter the depth
+				p[0].vertex.xyz = p[0].vertex.xyz / p[0].vertex.w; 
+
+				v[0].vertex = float4(p[0].vertex + halfS * right - halfS * up, 1);
+				v[1].vertex = float4(p[0].vertex + halfS * right + halfS * up, 1);
+				v[2].vertex = float4(p[0].vertex - halfS * right - halfS * up, 1);
+				v[3].vertex = float4(p[0].vertex - halfS * right + halfS * up, 1);
+
+				[unroll]
+				for (int i = 0; i < 4; i++)
+				{
+					v[i].screenpos = ComputeScreenPos(v[i].vertex);
+				}
+
+				//	#pragma warning( disable : 3078 ) // unity doesnt like trying to turn off the warning
+
+				[unroll]
+				for (int i = 0; i < 4; i++)
+				{
+					v[i].colour = p[0].colour;
+					v[i].index = p[0].index;
+					v[i].flags = p[0].flags;
+				}
+
+				triStream.Append(v[0]);
+				triStream.Append(v[1]);
+				triStream.Append(v[2]);
+				triStream.Append(v[3]);
+			}
+
+			fixed4 frag(g2f i) : SV_Target
+			{
+				flags[i.index] = i.flags;
+
+				fixed4 col = i.colour;
+				
+				if (CullBackfacing) 
+				{
+					if (i.flags & 2)
+					{
+						col.a = 0;
+					}
+				}
+
+				return col;
+			}
+
+			ENDCG
+		}
+	}
+}
